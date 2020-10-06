42f08a59930337d461e75499296316797f50ed26
leocassiosilva <leocassiosilva1234@gmail.com>
2020-10-06 10:18:14 -0300

    primeiro commit

---
+++ src/main/java/br/com/spring/repositorio/RepositorioObjetosAprendizagemApplication.java
@@ -0,0 +1,13 @@
+package br.com.spring.repositorio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepositorioObjetosAprendizagemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepositorioObjetosAprendizagemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/spring/repositorio/RepositorioObjetosAprendizagemApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.spring.repositorio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepositorioObjetosAprendizagemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
